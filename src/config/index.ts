45831aa70576796bcf4e480c18eb2e46fec44a2a
Praveen Janakarajan <praveen.41128@gmail.com>
2020-11-21 21:32:17 +0100

    Mandate node environment

--- src/config/index.ts
+++ src/config/index.ts
@@ -2,7 +2,11 @@ import { config } from "dotenv";
 
 export default () => {
   config()
-  const env: string = (process.env.NODE_ENV || "dev").toLowerCase();
+
+  const env: string = (process.env.NODE_ENV || '').toLowerCase();
+  if (!env) {
+    throw new Error('Node environment is missing!')
+  }
   return {
     env,
     ...((require(`./${env}.js`))(process.env))
