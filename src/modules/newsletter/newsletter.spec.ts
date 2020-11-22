a3f5ce886dd5d3e6cf3a9f8e50d01b009e8f21b8
Praveen Janakarajan <praveen.41128@gmail.com>
2020-11-22 19:59:06 +0100

    Create newsletter from user's subreddits

---
+++ src/modules/newsletter/newsletter.spec.ts
@@ -0,0 +1,55 @@
+import { resetDB } from "../../db";
+import { emailNewsletterForUser } from "./service";
+import { getApp } from "../../app";
+import { UUID } from "../../types/UUID";
+import supertest = require("supertest");
+
+jest.mock('node-fetch')
+
+const request = supertest(getApp())
+
+describe('Newsletter creation', () => {
+  beforeEach(resetDB)
+  afterAll(resetDB)
+
+  let user: UUID
+  beforeEach(async () => {
+    user = await request
+      .post('/users')
+      .send({
+        name: 'test',
+        email: 'test@some.com'
+      })
+      .then(response => response.body.data.id)
+    await request.post('/subreddits/investing/subscribe').send({user})
+    await request.post('/subreddits/eyebleach/subscribe').send({user})
+    await request.post('/subreddits/factorio/subscribe').send({user})
+  })
+
+  it('should create newsletter', async () => {
+    const subredditTest = (name: string) => {
+      return expect.objectContaining({
+        url: `https://www.reddit.com/r/${name}`,
+        posts: expect.arrayContaining([{
+          link: expect.any(String),
+          thumbnail: expect.any(String),
+          title: expect.any(String),
+        }, {
+          link: expect.any(String),
+          thumbnail: undefined,
+          title: expect.any(String),
+        }])
+      })
+    }
+    const newsletter = await emailNewsletterForUser(user)
+    expect(newsletter).toEqual(expect.objectContaining({
+      user: 'test',
+      email: 'test@some.com',
+      subreddits: expect.arrayContaining([
+        subredditTest('investing'),
+        subredditTest('eyebleach'),
+        subredditTest('factorio'),
+      ]),
+    }))
+  })
+})
---
+++ src/modules/newsletter/service.ts
@@ -0,0 +1,33 @@
+import User from "../users/User.model";
+import { getTopPostsInLastDay } from "../posts/service";
+import { UUID } from "../../types/UUID";
+
+const createNewsletterForUser = async (user: User) => {
+  const subreddits = await user.$get('subreddits')
+  const states = await Promise.all(subreddits.map(subreddit => getTopPostsInLastDay(subreddit)))
+  return {
+    user: user.name,
+    email: user.email,
+    subreddits: states
+      .filter(state => state.posts.length > 0)
+      .map(state => {
+        return {
+          url: state.link,
+          posts: state.posts
+        }
+      })
+  }
+}
+
+export const emailNewsletterForUser = async (userId: UUID) => {
+  const user = await User.findOne({where: {id: userId}})
+  if (!user) {
+    return
+  }
+  const newsletter = await createNewsletterForUser(user)
+
+  console.log('Dispatching newsletter for', newsletter.user, 'to', newsletter.email)
+  console.log(JSON.stringify(newsletter, null, 2))
+
+  return newsletter
+}
